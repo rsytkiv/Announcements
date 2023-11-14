f7bd8b8af0c4121d7faf73b7d06ee68d26664ad4
Roman Sytkiv <67324068+rsytkiv@users.noreply.github.com>
2023-11-14 23:13:43 +0200

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,25 +1,28 @@
 import React, { useState } from 'react'
-import classes from './Main.module.css';
 import { useSelector  } from 'react-redux';
-import AdItem from '../AdItem/AdItem'
 
-const Main = (props) => {
+import AdItem from '../AdItem/AdItem';
+
+import classes from './Main.module.css';
+
+const Main = () => {
     const [search, setSearch] = useState('');
     const items = useSelector((state) => state.ads.ads)
 
-    const filtered = items.filter(item => {
-        return item.title.toLowerCase().includes(search.toLowerCase())
-    });
+    const filteredItems = items.filter(item => item.title.toLowerCase().includes(search.toLowerCase()));
     return (
         <div className={classes.Container}>
-            <input type='text' className={classes.SearchInput} placeholder='Search...' onChange={(e) => setSearch(e.target.value)} />
+            <input
+                type='text'
+                className={classes.SearchInput}
+                placeholder="Search..."
+                onChange={(e) => setSearch(e.target.value)}
+            />
             <div className={classes.Posts}>
-                {items && filtered.map(item => 
-                    <AdItem  key={item.id} item={item}/>
-                )}
+                {items && filteredItems.map(item => <AdItem key={item.id} item={item} />)}
             </div>
         </div>
     )
 }
 
-export default Main
+export default Main;
